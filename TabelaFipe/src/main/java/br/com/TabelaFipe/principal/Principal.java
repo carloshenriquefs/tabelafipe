731f123fa44ea7a9916f9d1a4f58739f882e5869
Carlos Henrique <ichfs9108@gmail.com>
2024-01-30 17:23:03 -0300

    Modelando as classes da aplicacao

--- TabelaFipe/src/main/java/br/com/TabelaFipe/principal/Principal.java
+++ TabelaFipe/src/main/java/br/com/TabelaFipe/principal/Principal.java
@@ -1,13 +1,19 @@
 package br.com.TabelaFipe.principal;
 
+import br.com.TabelaFipe.model.Dados;
+import br.com.TabelaFipe.model.Modelos;
 import br.com.TabelaFipe.service.ConsumoApi;
+import br.com.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
 
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
@@ -37,6 +43,20 @@ public class Principal {
 
         var json = consumo.obterDados(endereco);
         System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para consulta: ");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterLista(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
 
     }
 }
